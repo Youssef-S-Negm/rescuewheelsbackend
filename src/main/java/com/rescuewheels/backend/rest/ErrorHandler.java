496b33d923e1e32a51ff7ba3cba1eaac9b03a0ee
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-28 23:10:49 +0200

    update: add vehicle not found exception handler

--- src/main/java/com/rescuewheels/backend/rest/ErrorHandler.java
+++ src/main/java/com/rescuewheels/backend/rest/ErrorHandler.java
@@ -2,6 +2,7 @@ package com.rescuewheels.backend.rest;
 
 import com.rescuewheels.backend.dto.ErrorResponse;
 import com.rescuewheels.backend.exception.UserNotFoundException;
+import com.rescuewheels.backend.exception.VehicleNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,6 +13,17 @@ public class ErrorHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException exception) {
+        ErrorResponse response = new ErrorResponse(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND.value(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleVehicleNotFoundException(VehicleNotFoundException exception) {
         ErrorResponse response = new ErrorResponse(
                 exception.getMessage(),
                 HttpStatus.NOT_FOUND.value(),
