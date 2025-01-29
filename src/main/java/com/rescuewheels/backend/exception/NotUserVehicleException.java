783d2590557dc3d5ba6f96e92da8b8fc375d55a7
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-29 19:35:04 +0200

    feat: add not user vehicle exception

---
+++ src/main/java/com/rescuewheels/backend/exception/NotUserVehicleException.java
@@ -0,0 +1,7 @@
+package com.rescuewheels.backend.exception;
+
+public class NotUserVehicleException extends RuntimeException {
+    public NotUserVehicleException(String message) {
+        super(message);
+    }
+}
