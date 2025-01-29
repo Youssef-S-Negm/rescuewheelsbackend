cac107b49629c0b0dda39390ef97efe61d6cf5c9
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-29 19:35:48 +0200

    update: add not user vehicle exception handler, global exception handler

--- src/main/java/com/rescuewheels/backend/rest/ErrorHandler.java
+++ src/main/java/com/rescuewheels/backend/rest/ErrorHandler.java
@@ -1,6 +1,7 @@
 package com.rescuewheels.backend.rest;
 
 import com.rescuewheels.backend.dto.ErrorResponse;
+import com.rescuewheels.backend.exception.NotUserVehicleException;
 import com.rescuewheels.backend.exception.UserNotFoundException;
 import com.rescuewheels.backend.exception.VehicleNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -31,5 +32,27 @@ public class ErrorHandler {
         );
 
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleNotUserVehicleException(NotUserVehicleException exception) {
+        ErrorResponse response = new ErrorResponse(
+                exception.getMessage(),
+                HttpStatus.FORBIDDEN.value(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        ErrorResponse response = new ErrorResponse(
+                exception.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
