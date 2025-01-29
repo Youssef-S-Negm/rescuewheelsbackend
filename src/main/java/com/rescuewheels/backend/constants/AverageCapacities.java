3590defb98ff2bd6e2c8c24af132f287140fb4ee
Youssef-S-Negm <youssef.negm2001@gmail.com>
2025-01-30 00:22:09 +0200

    feat: add global constants

---
+++ src/main/java/com/rescuewheels/backend/constants/AverageCapacities.java
@@ -0,0 +1,6 @@
+package com.rescuewheels.backend.constants;
+
+public class AverageCapacities {
+    public static double FUEL_TANK = 50.0; // in liters
+    public static double BATTERY_CAPACITY = 40.0; // in kWh
+}
---
+++ src/main/java/com/rescuewheels/backend/constants/PricingConstants.java
@@ -0,0 +1,8 @@
+package com.rescuewheels.backend.constants;
+
+public class PricingConstants {
+    public static final double PETROL_PRICE = 12.5; // Price per liter
+    public static final double DIESEL_PRICE = 10.5; // Price per liter
+    public static final double ELECTRIC_PRICE = 5.5; // Price per kWh
+    public static final double TRANSPORTATION_PRICE = 4.25; // Price per kilometer
+}
